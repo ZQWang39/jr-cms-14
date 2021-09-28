d7db75b4901ad166decf994ae227588e28231249
Ziqi Wang <ziqi.wang39@gmail.com>
2021-09-28 21:49:48 +1000

    Add listener to root path

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-require('express-async-errors');
-const errorHandler = require('./middleware/errorHandler');
-const validationErrorHandler = require('./middleware/validationErrorHandler');
-const routes = require('./routes');
+require("dotenv").config();
+const express = require("express");
+require("express-async-errors");
+const errorHandler = require("./middleware/errorHandler");
+const validationErrorHandler = require("./middleware/validationErrorHandler");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(express.json());
-
-app.use('/api', routes);
+app.get("/", (req, res) => {
+  res.send("Hello, there!");
+});
+app.use("/api", routes);
 
 app.use(validationErrorHandler);
 app.use(errorHandler);
